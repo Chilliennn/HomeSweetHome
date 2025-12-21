c293ffd7db91ff047b5102cad4b54d85de424f76
isZackYen <hansoonyen123@gmail.com>
2025-12-21 18:20:45 +0800

    Fix merge conflict in AdminViewModel index.ts

--- ViewModel/AdminViewModel/index.ts
+++ ViewModel/AdminViewModel/index.ts
@@ -1,8 +1,4 @@
 export { KeywordAdminViewModel, type ModalType } from './KeywordManagementViewModel';
-<<<<<<< HEAD
 export { AdminSentimentAnalysisViewModel, adminSentimentAnalysisViewModel } from './SentimentAnalysisViewModel';
-=======
-// SentimentAnalysisViewModel is exported from MatchingViewModel to avoid conflicts
->>>>>>> cfaaba15673471b1afdfba2ebcbd20085b3193b0
 export { adminViewModel, AdminViewModel } from './AdminViewModel';
 export { consultationViewModel, ConsultationViewModel } from './consultationViewModel';
